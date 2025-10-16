44b59d8ec8862657ed536961f83c58cd33855068
akovr <akovr@yahoo.com>
2025-10-16 10:52:03 +0700

    внесены правки 2: исправлен падающий тест, добавлена очистка полей

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -1,6 +1,8 @@
 package page;
 
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
@@ -13,14 +15,24 @@ public class LoginPage {
     }
 
     public void login(String login, String password) {
-        $("[data-test-id='login'] .input__control").shouldBe(visible).setValue(login);
-        $("[data-test-id='password'] .input__control").shouldBe(visible).setValue(password);
+        SelenideElement loginInput = $("[data-test-id='login'] .input__control").shouldBe(visible);
+        SelenideElement passwordInput = $("[data-test-id='password'] .input__control").shouldBe(visible);
+
+        // Очистка полей: выделение всего текста и удаление
+        loginInput.sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.BACK_SPACE);
+        passwordInput.sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.BACK_SPACE);
+
+        // Ввод новых значений
+        loginInput.setValue(login);
+        passwordInput.setValue(password);
+
         $("[data-test-id='action-login']").shouldBe(visible).click();
     }
+
 
     public void shouldShowBlockageError() {
         $("[data-test-id='error-notification']")
                 .shouldBe(visible)
-                .shouldHave(text("блокирован"));
+                .shouldHave(text("Ошибка! Неверно указан логин или пароль"));
     }
 }
